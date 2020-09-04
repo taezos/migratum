50966783c46dd6f9fd98cb1f099dd25413d71735
piq9117 <piq9117@noreply.localhost>
2020-09-04 22:18:55 +0000

    hasql-migration override removed (#30)
    hasql-migration override removed
    
    Co-authored-by: ken <ken@pop-os.localdomain>
    Reviewed-on: https://taezos.org/taezos/migratum/pulls/30

--- release.nix
+++ release.nix
@@ -1,15 +1,4 @@
 let
   pkgs = import ./nix/pinned.nix {};
-
-  # remove this when haskell-updates branch with hasql-migration marked as
-  # unbroken is merged to master
-  haskellPackages = pkgs.haskellPackages.override {
-    overrides = self: super: rec {
-      hasql-migration = self.callCabal2nix "hasql-migration" ( builtins.fetchGit {
-        url = "git@github.com:tvh/hasql-migration.git";
-        rev = "58c90f7c5c0e829708c35db9d2c8bc47e86621eb";
-      }){};
-    };
-  };
 in
-  haskellPackages.callPackage ./default.nix {}
+  pkgs.haskellPackages.callPackage ./default.nix {}
